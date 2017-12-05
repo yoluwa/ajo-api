6403c09d112cf336b7090ebeeff8f0ea6d7a0417
Amoo Abeeb <amooabeeb0@gmail.com>
2017-12-05 13:18:38 +0100

    hash password

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -11,24 +11,23 @@ module.exports.index = function(req, res, next) {
 };
 
 module.exports.create = function(req, res, next) {
-    var hashed_password = bcrypt.hash(req.body.password,10, function(err, hash) {
-       return hash;
-    });
-    var user = new User({
-       name: req.body.name,
-       password: req.body.password,
-       email: req.body.email,
-       phone: req.body.phone,
-       bvn: req.body.bvn,
-       account_number: req.body.account_number,
-       security_question: req.body.security_question,
-       answer_to_security: req.body.answer_to_security,
-       date_of_birth: req.body.date_of_birth,
-       bank_name: req.body.bank_name
-    });
-    user.save().then(function(doc){
-        Response.sendSuccess(res,doc)
-    }, function(err){
-        Response.sendError(res, err)
+    bcrypt.hash(req.body.password,10, function(err, hash) {
+        var user = new User({
+            name: req.body.name,
+            password: hash,
+            email: req.body.email,
+            phone: req.body.phone,
+            bvn: req.body.bvn,
+            account_number: req.body.account_number,
+            security_question: req.body.security_question,
+            answer_to_security: req.body.answer_to_security,
+            date_of_birth: req.body.date_of_birth,
+            bank_name: req.body.bank_name
+        });
+        user.save().then(function(doc){
+            Response.sendSuccess(res,doc)
+        }, function(err){
+            Response.sendError(res, err)
+        });
     });
 };
